f1c4eb7bb78b4cb45b2274b4072673986f08b2d9
Saksham Garg <saksham2700@gmail.com>
2025-06-06 19:50:11 +0530

    added header, footer

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,7 @@
+import Facebook from "./facebook";
+import Linkedin from "./linkedin";
+import Instagram from "./instagram";
+import Twitter from "./twitter";
+import Youtube from "./youtube";
+
+export { Facebook, Linkedin, Instagram, Twitter, Youtube };
