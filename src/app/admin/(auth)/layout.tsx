3a20ca6a1d639441d6ed220d606304ffc8f8bb4d
Saksham Garg <saksham2700@gmail.com>
2025-06-10 14:22:07 +0530

    feat: added admin panel authentication

---
+++ src/app/admin/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const cookieStore = await cookies();
+	const token = cookieStore.get("token")?.value;
+
+	if (token) {
+		let isValidToken = false;
+		try {
+			const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+			if (decoded) {
+				isValidToken = true;
+			}
+		} catch (err) {
+			// Token invalid or expired
+			console.log("Token invalid or expired");
+		}
+
+		if (isValidToken) {
+			redirect("/admin");
+		}
+	}
+
+	return <>{children}</>;
+}
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,12 @@ import { ngoDetails } from "@/config/config";
 import { type MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    host: ngoDetails.contact.website,
-  };
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+			disallow: "/admin/",
+		},
+		host: ngoDetails.contact.website,
+	};
 }
