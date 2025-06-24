c86d2c2d6cb8c2be1fe118a949a395ec53e2dd90
Harshavardhan Balaka <harshavardhanbalaka@gmail.com>
2025-06-24 09:53:44 +0530

    fix: update about us page content

---
+++ src/config/team.ts
@@ -0,0 +1,26 @@
+export const teamembers = [
+	{
+		name: "Mr. Shivam Pathak",
+		role: "Director",
+		imageSrc: "/man.png",
+		linkedinUrl: "https://www.linkedin.com/in/ShivamPathak",
+	},
+	{
+		name: "Mrs. Pooja Pathakk",
+		role: "Director",
+		imageSrc: "/woman.png",
+		linkedinUrl: "https://www.linkedin.com/in/PoojaPathak",
+	},
+	{
+		name: "Mr. Nilesh Pal",
+		role: "Director",
+		imageSrc: "/man.png",
+		linkedinUrl: "https://www.linkedin.com/in/NileshPathak",
+	},
+	{
+		name: "Mr. Zeel Mangukiya",
+		role: "COO",
+		imageSrc: "/man.png",
+		linkedinUrl: "https://www.linkedin.com/in/ZeelMangukiya",
+	},
+];
