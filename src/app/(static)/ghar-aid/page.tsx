f8eefbcad72e01fa2480063745ef32af510482a6
Saksham Garg <sakshamgarg@Sakshams-MacBook-Air.local>
2025-07-14 18:12:12 +0530

    feat: added titles for aid pages

--- src/app/(static)/ghar-aid/page.tsx
+++ src/app/(static)/ghar-aid/page.tsx
@@ -6,6 +6,13 @@ import Medical from "@/components/aids/gharaid/medical";
 import Childsupport from "@/components/aids/gharaid/childsupport";
 import Live from "@/components/aids/gharaid/live";
 import Yourhelp from "@/components/aids/gharaid/yourhelp";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Ghar Aid",
+};
+
 function GharId() {
 	return (
 		<div>
