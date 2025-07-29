266bf93f912191c1715eefbc4933d10a77291d67
Harshavardhan Balaka <harshavardhanbalaka@gmail.com>
2025-07-29 23:45:21 +0530

    feat: update careers page

--- src/app/(static)/our-careers/page.tsx
+++ src/app/(static)/our-careers/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import React from "react";
 import { Container, Paper, Stack, Title, Divider } from "@mantine/core";
-import Name from "@/components/ourcareers/name";
+
 import Otherdetails from "@/components/ourcareers/otherdetails";
-import Uploadcv from "@/components/ourcareers/uploadcv";
-import Apply from "@/components/ourcareers/apply";
 
 function OurCareers() {
 	return (
