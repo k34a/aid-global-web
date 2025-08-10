0aa524a0d1e4b8760ebe27444753022e79a9d8f6
Harshavardhan Balaka <harshavardhanbalaka@gmail.com>
2025-08-10 21:14:55 +0530

    feat: use mantine for faqs

--- src/components/recurring-donations/faq/coins.tsx
+++ src/components/recurring-donations/faq/coins.tsx
@@ -1,16 +1,10 @@
 "use client";
+import { Accordion } from "@mantine/core";
+import React from "react";
 
-import React, { useState } from "react";
-import { ChevronDown } from "lucide-react";
 import { questionqa100 } from "@/config/faqquestions";
 
 function Question100() {
-	const [openIndex, setOpenIndex] = useState<number | null>(null);
-
-	const toggleItem = (index: number) => {
-		setOpenIndex(openIndex === index ? null : index);
-	};
-
 	return (
 		<div className="ml-4 mt-6 sm:ml-8 sm:mt-8 md:ml-12 md:mt-10 lg:ml-20 lg:mt-12 xl:ml-32 xl:mt-12 mr-4 sm:mr-5 md:mr-6 lg:mr-7 xl:mr-8">
 			<h2 className="text-blue-400 font-extrabold text-xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl">
@@ -31,60 +25,50 @@ function Question100() {
 				<h2 className="text-blue-400 font-medium text-base sm:text-xl md:text-xl lg:text-2xl mt-5">
 					FAQ The{"\u20B9"}100 Club
 				</h2>
-				<ul className="ml-1 sm:ml-2 md:ml-3 lg:ml-4 mt-3 mr-4 sm:mr-5 md:mr-5 lg:mr-6">
+
+				<Accordion className="ml-1 sm:ml-2 md:ml-3 lg:ml-4 mt-3 mr-4 sm:mr-5 md:mr-5 lg:mr-6">
 					{questionqa100.map((item, index) => (
-						<li
-							key={index}
-							onClick={() => toggleItem(index)}
-							className="text-base sm:text-xl md:text-xl lg:text-2xl opacity-60 font-semibold bg-white shadow-lg border-black rounded-xl p-3 mb-5 cursor-pointer"
-						>
-							<div className="flex items-center justify-between pl-1 sm:pl-2 md:pl-3 lg:pl-4">
-								<span>{item.question}</span>
-								<ChevronDown
-									className={`transition-transform duration-300 ${
-										openIndex === index ? "rotate-180" : ""
-									}`}
-								/>
-							</div>
-							{openIndex === index && (
-								<div className="mt-3 text-base pl-1 sm:pl-2 md:pl-3 lg:pl-4 sm:text-base md:text-lg lg:text-lg leading-relaxed opacity-85 whitespace-pre-line">
-									{item.type === "contact" ? (
-										<>
-											Visit{" "}
-											<a
-												href="https://www.aidglobal.ngo/1rupee"
-												target="_blank"
-												rel="noopener noreferrer"
-												className="text-blue-500 underline"
-											>
-												aidglobal.ngo/1rupee
-											</a>{" "}
-											to sign up or learn more.
-											<br />
-											Email:{" "}
-											<a
-												href="mailto:info@aidglobal.ngo"
-												className="text-blue-500 underline"
-											>
-												info@aidglobal.ngo
-											</a>
-											<br />
-											Phone:{" "}
-											<a
-												href="tel:+919373469754"
-												className="text-blue-500 underline"
-											>
-												+91-9373469754
-											</a>
-										</>
-									) : (
-										item.answer
-									)}
-								</div>
-							)}
-						</li>
+						<Accordion.Item key={index} value={item.question}>
+							<Accordion.Control className="sm:text-base md:text-lg lg:text-lg">
+								{item.question}
+							</Accordion.Control>
+							<Accordion.Panel className="mt-3 text-base pl-1 sm:pl-2 md:pl-3 lg:pl-4 sm:text-base md:text-lg lg:text-lg leading-relaxed opacity-85 whitespace-pre-line">
+								{item.type === "contact" ? (
+									<>
+										Visit{" "}
+										<a
+											href="https://www.aidglobal.ngo/1rupee"
+											target="_blank"
+											rel="noopener noreferrer"
+											className="text-blue-500 underline"
+										>
+											aidglobal.ngo/1rupee
+										</a>{" "}
+										to sign up or learn more.
+										<br />
+										Email:{" "}
+										<a
+											href="mailto:info@aidglobal.ngo"
+											className="text-blue-500 underline"
+										>
+											info@aidglobal.ngo
+										</a>
+										<br />
+										Phone:{" "}
+										<a
+											href="tel:+919373469754"
+											className="text-blue-500 underline"
+										>
+											+91-9373469754
+										</a>
+									</>
+								) : (
+									item.answer
+								)}
+							</Accordion.Panel>
+						</Accordion.Item>
 					))}
-				</ul>
+				</Accordion>
 
 				<div className="bg-gray-100 rounded-xl shadow-md p-5 mt-8">
 					<h3 className="text-lg sm:text-xl font-semibold mb-3 flex items-center gap-2">
