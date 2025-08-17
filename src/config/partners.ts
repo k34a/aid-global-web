f379c76265c33a853b1528183355f82794c9d54e
asiya-tabasum <asiyashaik7867@gmail.com>
2025-08-17 17:00:46 +0530

    fix: donate button and partners

--- src/config/partners.ts
+++ src/config/partners.ts
@@ -3,10 +3,50 @@ import { STATIC_IMAGE_HOST } from "@/config/config";
 export const partners = [
 	{
 		name: "Rise Against Hunger",
-		imageSrc: `${STATIC_IMAGE_HOST}partners/partner-rise.webp`,
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/partner-rise.webp`,
 	},
 	{
 		name: "give",
-		imageSrc: `${STATIC_IMAGE_HOST}partners/partner-give.webp`,
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/partner-give.webp`,
+	},
+	{
+		name: "chezuba",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/chezuba.webp`,
+	},
+	{
+		name: "connectfor",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/connectfor.webp`,
+	},
+	{
+		name: "donatekart",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/donatekart.webp`,
+	},
+	{
+		name: "give India",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/giveIndia.webp`,
+	},
+	{
+		name: "guidestar",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/guidestar.webp`,
+	},
+	{
+		name: "karmpoints",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/karm.webp`,
+	},
+	{
+		name: "rotaract",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/rotaract.webp`,
+	},
+	{
+		name: "rotary",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/rotary.webp`,
+	},
+	{
+		name: "united",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/united.webp`,
+	},
+	{
+		name: "clubfoot",
+		imageSrc: `${STATIC_IMAGE_HOST}partners/partners/clubfootsols.webp`,
 	},
 ];
